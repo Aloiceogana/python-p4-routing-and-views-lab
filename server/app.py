c11e63fc7ddca0936bb2077eef5737d3f2aa2c97
Aloice Ogana <aloiceadriot@gmail.com>
2024-07-06 15:44:06 +0300

    Completed Flask routing and views lab

--- server/app.py
+++ server/app.py
@@ -1,37 +1,37 @@
 
+
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Python Operations with Flask Routing and Views</h1>'
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
 
 @app.route('/print/<string:parameter>')
-def print_string(parameter):
-    print(parameter)
-    return parameter  # Return plain text without HTML tags
+def print_parameter(parameter):
+    print(f"{parameter}")
+    return parameter
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
-    numbers = '<br>'.join(str(i) for i in range(parameter))
-    return f'<h1>{numbers}</h1>'
+    numbers = [i for i in range(int(parameter))]
+    return "\n".join(map(str, numbers)) + '\n' 
 
-@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
-def math(num1, operation, num2):
-    if operation == '+':
-        result = num1 + num2
-    elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return '<h1>Invalid operation</h1>'
-    return f'<h1>{num1} {operation} {num2} = {result}</h1>'
+@app.route('/math/<int:parameter1>/<string:operation>/<int:parameter2>')
+def math(parameter1, operation, parameter2):
+    if operation == "+":
+        return str(parameter1 + parameter2)
+    elif operation == "-":
+        return str(parameter1 - parameter2)
+    elif operation == "*":
+        return str(parameter1 * parameter2)
+    elif operation == "div":
+        return str(parameter1 / parameter2)
+    elif operation == "%":
+        return str(parameter1 % parameter2)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
